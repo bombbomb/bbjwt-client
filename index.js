ab5de39068bd30f9d2e415806665afad4c4757e7
bbouley <bradleyjamesbouley@gmail.com>
2017-02-21 13:44:27 -0700

    Built new functionality to handle V2 secret as well as original

--- index.js
+++ index.js
@@ -1,27 +1,60 @@
 var jwt = require('jsonwebtoken');
 
 module.exports = {
-    getClientIdFromToken: function(jwt) {
+    getClientIdFromToken: function(jwt)
+    {
         var payload = this.decodeToken(jwt);
-        if (payload === false) {
+        if (payload === false)
+        {
             return false;
         }
         return payload.clientId;
     },
 
-    decodeToken: function(token) {
-        try {
-            var decoded = jwt.verify(token, process.env.JWT_SECRET);
+    decodeToken: function(token)
+    {
+        var decoded = false;
+        try
+        {
+            decoded = this.decodeV1Token(token);
+            if(decoded) { return decoded; }
+        }
+        catch(err)
+        {
+            console.log("JWT V1 decode failed: " + token);
+        }
+        try
+        {
+            decoded = this.decodeV2Token(token);
+        }
+        catch(err)
+        {
+            console.log("JWT V2 decode failed: " + token);
+        }
+        return decoded;
+    },
 
-            if (!decoded.hasOwnProperty('expires') || decoded.expires < Date.now()/1000) {
-                console.log("JWT token expired failed: " + token);
-                return false;
-            }
-
-            return decoded;
-        } catch(err) {
-            console.log("JWT decode failed: " + token);
+    decodeV1Token: function(token)
+    {
+        var decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if (!decoded.hasOwnProperty('expires') || decoded.expires < Date.now()/1000) {
+            console.log("JWT token expired failed: " + token);
             return false;
         }
+        return decoded;
+    },
+
+    decodeV2Token: function(token)
+    {
+        var decoded = jwt.verify(token, process.env.JWT_V2_SECRET);
+        if (!decoded.hasOwnProperty('expires') || decoded.expires < Date.now()/1000) {
+            console.log("JWT token expired failed: " + token);
+            return decoded;
+        }
+        return decoded;
     }
 };
+
+
+
+
